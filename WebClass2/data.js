da65b9660616c3c078cdf20239ec9566cd71cbea
Rutansh chawla <rutansh3456@gmail.com>
2025-07-26 10:47:38 +0530

    Web class2

---
+++ WebClass2/data.js
@@ -0,0 +1,12 @@
+ProductListFromServer = [
+    {
+        name: "Product 1",
+        price: "$10",
+        description: "It is a good product",
+    },
+    {
+        name: "Product 2",
+        price: "$20",
+        description: "It is even better product",
+    },
+];
